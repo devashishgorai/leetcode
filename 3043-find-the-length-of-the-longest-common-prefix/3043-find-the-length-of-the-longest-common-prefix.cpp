82bad6f280836171405f6806ed6494edeb46d749
Devashish Gorai <devashishgorai10@gmail.com>
2026-07-04 20:39:41 +0530

    Time: 135 ms (76.44%), Space: 169.4 MB (21.99%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
+    struct Node {
+        Node* child[10];
+        bool isEnd;
+        Node(){
+            for(int i=0;i<10;i++)child[i]=nullptr;
+            isEnd=false;
+        }
+    };
+    void insert(string word, Node* root){
+        Node* temp=root;
+        for(char ch:word){
+            int idx=ch-'0';
+            if (temp->child[idx] == nullptr){
+                temp->child[idx] = new Node();
+            }
+            temp=temp->child[idx];
+        }
+        temp->isEnd=true;
+    }
+
+    int check(string str, Node* root) {
+        Node* temp = root;
+        int idx = 0;
+        while (idx < str.size()) {
+            int i = str[idx] - '0';
+            if (temp->child[i] != nullptr) {
+                temp = temp->child[i];
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return idx;
+    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-
-        unordered_set<string> prefixes;
-
-        // Store all prefixes of arr1
-        for (int num : arr1) {
-
-            string s = to_string(num);
-            string prefix = "";
-
-            for (char ch : s) {
-                prefix += ch;
-                prefixes.insert(prefix);
-            }
+        Node* root = new Node();
+        for(int x:arr2){
+            string str=to_string(x);
+            insert(str,root);
         }
-
-        int ans = 0;
-
-        // Check prefixes of arr2
-        for (int num : arr2) {
-
-            string s = to_string(num);
-            string prefix = "";
-
-            for (char ch : s) {
-
-                prefix += ch;
-
-                if (prefixes.count(prefix))
-                    ans = max(ans, (int)prefix.length());
-                else
-                    break;
-            }
+        int ans=0;
+        for(int x:arr1){
+            string str=to_string(x);
+            ans=max(ans,check(str, root));
         }
-
         return ans;
     }
 };
