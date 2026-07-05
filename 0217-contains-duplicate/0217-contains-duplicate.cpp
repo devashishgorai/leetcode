aaecaa07bca2537780130600cc0213900b2f7484
Devashish Gorai <devashishgorai10@gmail.com>
2026-07-05 09:48:14 +0530

    Time: 72 ms (18.81%), Space: 111.4 MB (6.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-
-        unordered_set<int> seen;   // Pehle se dekhe hue numbers store karega
-
-        // Array ka har element check karo
+        unordered_map<int, int> seen;
         for (int num : nums) {
-
-            // Agar number pehle se set me hai
-            if (seen.find(num) != seen.end()) {
-                return true;        // Duplicate mil gaya
-            }
-
-            // Nahi mila to set me insert kar do
-            seen.insert(num);
+            if (seen[num] >= 1)
+                return true;
+            seen[num]++;
         }
-
-        // Pura array check ho gaya, duplicate nahi mila
         return false;
     }
 };
