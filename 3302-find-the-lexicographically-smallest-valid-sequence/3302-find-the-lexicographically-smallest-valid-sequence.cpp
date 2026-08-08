eee19e913028198358c6301ba6f9c9b93027d937
Devashish Gorai <devashishgorai10@gmail.com>
2026-08-08 22:10:10 +0530

    Time: 48 ms (41.49%), Space: 96.9 MB (90.43%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    vector<int> validSequence(string s, string t) {
-        // we need lexicographically smallest... means we have to choose the min index if possible. 
-
-        // so we need to know at each index,  if with the suffix is it possible to make the string atmost equal to word2 or not? 
-        int n = s.length();
-        int m = t.length();
-        int i = n - 1, j= m - 1; 
-        vector<int> dp(n + 1, 0); 
-        for(; i >= 0; i--) {
-            if(j >= 0 && s[i] == t[j]) {
-                // if these are equal 
-                dp[i] = dp[i + 1] + 1; 
-                j--; 
-            } else dp[i] = dp[i + 1]; 
+    vector<int> validSequence(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<int> suf(n + 1, -1);
+        suf[n] = m;
+        for (int i = m - 1, j = n - 1; i >= 0; i--) {
+            if (word1[i] == word2[j]) suf[j--] = i;
+            if (j < 0) break;
         }
-
-        // Now for each index greedily I have to tell If I can form the answer or not. 
-        vector<int> ans; 
-        j = 0; 
-        bool flag = true; // means match available 
-        for(int i = 0; i < n && j < m; i++) {
-            int rem = m - j - 1; // if we pick cur 
-            // match possible in suffix dp[i + 1], else possible what can I choose? 
-            if( s[i] == t[j]) {
-                // means both are equal 
-                ans.push_back(i); // adding this will form lexicographical string. 
-                j++; 
-            } else if(flag && dp[i + 1] >= rem) {
-                // words are not equal 
-                // I have to make a choice 
-                // If i change cur than for next index ... what is the suffix length which is still possible? 
-
-                // then I can choose this means i can still form rest of the string 
-                ans.push_back(i); 
-                j++; 
-                flag = false; 
+        vector<int> res(n);
+        bool changed = false;
+        for (int i = 0, j = 0; i < m; i++) {
+            if (word1[i] == word2[j]) res[j++] = i;
+            else if (!changed && i < suf[j + 1]) {
+                changed = true;
+                res[j++] = i;
             }
+            if (j == n) return res;
         }
-
-        if (j == m) return ans; 
-        return {}; 
+        return {};
     }
 };
